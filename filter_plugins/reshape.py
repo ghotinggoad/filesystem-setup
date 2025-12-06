968b81395fda7a83a41fbd58c7122607a2ff00aa
Rod Oh Zhi Hua <rodohzhihua@gmail.com>
2025-12-06 15:10:39 +0800

    refactor: identify cloud and map symlinks using ansible_facts instead of commands

--- filter_plugins/reshape.py
+++ filter_plugins/reshape.py
@@ -1,3 +1,15 @@
+def to_symlink_map(ansible_facts):
+    virtualization_type = ansible_facts.get("virtualization_type")
+    system_vendor = ansible_facts.get("system_vendor")
+    if virtualization_type == "kvm" and system_vendor == "QEMU":
+        symlink_map = {
+            device.get("links").get("ids")[0][-5:]: "/dev/" + device_key
+            for device_key, device in ansible_facts.get("devices").items()
+            if device.get("model") == "QEMU HARDDISK"
+        }
+        return symlink_map
+
+
 def to_lvs(vdisks):
     lvs = {
         persistent_mount_name: {**persistent_mount, "vdisk_name": vdisk_name}
@@ -11,4 +23,4 @@ def to_lvs(vdisks):
 
 class FilterModule(object):
     def filters(self):
-        return {"to_lvs": to_lvs}
+        return {"to_symlink_map": to_symlink_map, "to_lvs": to_lvs}
